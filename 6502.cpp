3581189c53b2a9bb6e79c0a4e2354d47ab5ad005
Groffa <jens@pushingcows.se>
2016-03-29 14:30:36 +0200

    Changed test program

--- 6502.cpp
+++ 6502.cpp
@@ -11,14 +11,12 @@ main(int argc, char *argv[])
     ram_t Ram = {0};
 
     u8 Program[] = {
-        ORA__X, 0, 1
+        BRK
     };
 
     u8 ProgramCount = sizeof(Program) / sizeof(Program[0]);
 
     LoadProgram(&Ram, Program, ProgramCount);
-    Cpu.X = 0; 
-    Cpu.A = 255;
     SingleStepProgram(&Cpu, &Ram);
 
     return 0;
