4145897e5318fcb8d6dafb611e1aad134fc36d45
Groffa <jens@pushingcows.se>
2016-03-29 20:57:14 +0200

    Added program loading from file.

--- 6502.cpp
+++ 6502.cpp
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "6502.h"
 
 // For testing purposes
 #include "opcodes.h"
+
+static bool
+LoadProgram(ram_t *Ram, const char *Filename)
+{
+    FILE *File = fopen(Filename, "rb");
+    bool Result = false;
+    if (File) {
+        Result = true;
+        fseek(File, 0, SEEK_END);
+        u16 ProgramSize = ftell(File);
+        fseek(File, 0, SEEK_SET);
+        u8 *Program = (u8 *)calloc(ProgramSize, 1);
+        fread(Program, 1, ProgramSize, File);
+        fclose(File);
+        free(Program);
+
+        LoadProgram(Ram, Program, ProgramSize);
+    }
+    return Result;
+}
 
 int
 main(int argc, char *argv[])
