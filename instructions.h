fd323800bc309b7d798114cfeda0e0417530e0d5
Groffa <jens@pushingcows.se>
2016-04-10 19:30:30 +0200

    Quick fix to get all opcodes into the program

--- instructions.h
+++ instructions.h
@@ -34,6 +34,168 @@ static instruction_t InstructionTable[] = {
     OPCODE(ASL_zpg, 1, 5),
     OPCODE(PHP, 0, 3),
     OPCODE(PHA, 0, 3),
+
+    // Below: NOT IMPLEMENTED
+    OPCODE(ORA_im,1,0),             // ORA immediate
+    OPCODE(ASL,0,0),                // ASL accumulator
+    OPCODE(ORA_abs,2,0),
+    OPCODE(ASL_abs,2,0),            // ASL absolute
+    
+    OPCODE(BPL_rel,1,0),
+    OPCODE(ORA_ind_Y,1,0),          // ORA (zeropage), Y
+    OPCODE(ORA_zpg_X,1,0),
+    OPCODE(ASL_zpg_X,1,0),          // ASL zeropage, X
+    OPCODE(CLC,0,0),
+    OPCODE(ORA_abs_Y,0,0),          // ORA absolute, Y
+    OPCODE(ORA_abs_X,2,0),
+    OPCODE(ASL_abs_X,2,0),          // ASL absolute, X
+    
+    OPCODE(JSR,2,0),
+    OPCODE(AND_X_ind,1,0),          // AND (zeropage, X)
+    OPCODE(BIT_zpg,1,0),
+    OPCODE(AND_zpg,1,0),            // AND zeropage
+    OPCODE(ROL_zpg,1,0),            // ROL zeropage
+    OPCODE(PLP ,0,0),
+    OPCODE(AND_im,1,0),             // AND immediate
+    OPCODE(ROL,0,0),                // ROL accumulator,
+    OPCODE(BIT_abs,2,0),
+    OPCODE(AND_abs,2,0),            // AND absolute
+    OPCODE(ROL_abs,2,0),            // ROL absolute
+
+    OPCODE(BMI_rel,1,0),
+    OPCODE(AND_ind_Y,1,0),          // AND (zeropage), Y
+    OPCODE(AND_zpg_X,1,0),
+    OPCODE(ROL_zpg_X,1,0),          // ROL zeropage, X
+    OPCODE(SEC ,0,0),
+    OPCODE(AND_abs_Y,2,0),          // AND absolute, Y
+    OPCODE(AND_abs_X,2,0),
+    OPCODE(ROL_abs_X,2,0),          // ROL absolute, X
+
+    OPCODE(RTI ,0,0),
+    OPCODE(EOR_X_ind,1,0),          // EOR (zeropage, X)
+    OPCODE(EOR_zpg,1,0),
+    OPCODE(LSR_zpg,1,0),            // LSR zeropage
+    OPCODE(EOR_im,1,0),             // EOR immediate
+    OPCODE(LSR,0,0),                // LSR accumulator
+    OPCODE(JMP_abs,2,0),
+    OPCODE(EOR_abs,2,0),            // EOR absolute
+    OPCODE(LSR_abs,2,0),            // LSR absolute
+
+    OPCODE(BVC_rel,1,0),
+    OPCODE(EOR_ind_Y,1,0),          // EOR (zeropage), Y
+    OPCODE(EOR_zpg_X,1,0),
+    OPCODE(LSR_zpg_X,1,0),          // LSR zeropage, X
+    OPCODE(CLI ,0,0),
+    OPCODE(EOR_abs_Y,2,0),          // EOR absolute, Y
+    OPCODE(EOR_abs_X,2,0),
+    OPCODE(LSR_abs_X,2,0),          // LSR absolute, X
+
+    OPCODE(RTS,0,0),
+    OPCODE(ADC_X_ind,1,0),          // ADC (zeropage, X)
+    OPCODE(ADC_zpg,1,0),
+    OPCODE(ROR_zpg,1,0),            // ROR zeropage
+    OPCODE(PLA,0,0),
+    OPCODE(ADC_im,1,0),             // ADC immediate
+    OPCODE(ROR,0,0),                // ROR accumulator
+    OPCODE(JMP_ind,2,0),
+    OPCODE(ADC_abs,2,0),            // ADC absolute
+    OPCODE(ROR_abs,2,0),            // ROR absolute
+
+    OPCODE(BVS_rel,1,0),
+    OPCODE(ADC_ind_Y,1,0),          // ADC (zeropage), Y
+    OPCODE(ADC_zpg_X,1,0),
+    OPCODE(ROR_zpg_X,1,0),          // ROR zeropage, X
+    OPCODE(SEI ,0,0),
+    OPCODE(ADC_abs_Y,2,0),          // ADC absolute, Y
+    OPCODE(ADC_abx_X ,0,0),
+    OPCODE(ROR_abs_X,2,0),          // ROR absolute, X
+
+    OPCODE(STA_X_ind,1,0),
+    OPCODE(STY_zpg,1,0),
+    OPCODE(STA_zpg,1,0),            // STA zeropage
+    OPCODE(STX_zpg,1,0),            // STX zeropage
+    OPCODE(DEY ,0,0),
+    OPCODE(TXA ,0,0),
+    OPCODE(STY_abs,2,0),
+    OPCODE(STA_abs,2,0),            // STA absolute,
+    OPCODE(STX_abs,2,0),            // STX absolute
+
+    OPCODE(BCC_rel,1,0),
+    OPCODE(STA_ind_Y,1,0),          // STA (zeropage), Y
+    OPCODE(STY_zpg_X,1,0),
+    OPCODE(STA_zpg_X,1,0),          // STA zeropage, X
+    OPCODE(STX_zpg_Y,0,0),          // STX zeropage, Y
+    OPCODE(TYA ,0,0),
+    OPCODE(STA_abs_Y,2,0),          // STA absolute, Y
+    OPCODE(TXS,0,0),                // TXS
+    OPCODE(STA_abs_X,2,0),
+
+    OPCODE(LDY_im,1,0),
+    OPCODE(LDA_X_ind,1,0),          // LDA (zeropage, X)
+    OPCODE(LDX_im,1,0),             // LDX immediate
+    OPCODE(LDY_zpg,1,0),
+    OPCODE(LDA_zpg,1,0),            // LDA zeropage
+    OPCODE(LDX_zpg,1,0),            // LDX zeropage
+    OPCODE(TAY,0,0),
+    OPCODE(LDA_im,1,0),             // LDA immediate
+    OPCODE(TAX,0,0),                // TAX
+    OPCODE(LDY_abs,2,0),
+    OPCODE(LDA_abs,2,0),            // LDA absolute
+    OPCODE(LDX_abs,2,0),            // LDX absolute
+
+    OPCODE(BCS_rel,1,0),
+    OPCODE(LDA_ind_Y,1,0),          // LDA (zeropage), Y
+    OPCODE(LDY_zpg_X,1,0),
+    OPCODE(LDA_zpg_X,1,0),          // LDA zeropage, X
+    OPCODE(LDX_zpg_Y,0,0),          // LDX zeropage, Y
+    OPCODE(CLV,0,0),
+    OPCODE(LDA_abs_Y,2,0),          // LDA absolute, Y
+    OPCODE(TSX,0,0),                // TSX
+    OPCODE(LDY_abs_X,2,0),
+    OPCODE(LDA_abs_X,2,0),          // LDA absolute, X
+    OPCODE(LDX_abs_Y,2,0),          // LDX absolute, Y
+
+    OPCODE(CPY_im,1,0),
+    OPCODE(CMP_X_ind,1,0),          // CMP (zeropage, X)
+    OPCODE(CPY_zpg,1,0),
+    OPCODE(CMP_zpg,1,0),            // CMP zeropage
+    OPCODE(DEC_zpg,1,0),            // DEC zeropage
+    OPCODE(INY,0,0),
+    OPCODE(CMP_im,1,0),             // CMP immediate
+    OPCODE(DEX,0,0),                // DEX
+    OPCODE(CPY_abs,2,0),
+    OPCODE(CMP_abs,2,0),            // CMP absolute
+    OPCODE(DEC_abs,2,0),            // DEC absolute
+
+    OPCODE(BNE_rel,1,0),
+    OPCODE(CMP_ind_Y,1,0),          // CMP (zeropage), Y
+    OPCODE(CMP_zpg_X,1,0),
+    OPCODE(DEC_zpg_X,1,0),          // DEC zeropage, X
+    OPCODE(CLD ,0,0),
+    OPCODE(CMP_abs_Y,2,0),          // CMP absolute, Y
+    OPCODE(CMP_abs_X,2,0),
+    OPCODE(DEC_abs_X,2,0),          // DEC absolute, X
+
+    OPCODE(CPX_im,1,0),
+    OPCODE(SBC_X_ind,1,0),          // SBC (zeropage, X)
+    OPCODE(CPX_zpg,1,0),
+    OPCODE(SBC_zpg,1,0),            // SBC zeropage
+    OPCODE(INC_zpg,1,0),            // INC zeropage
+    OPCODE(INX,0,0),
+    OPCODE(SBC_im,1,0),             // SBC immediate
+    OPCODE(NOP,0,0),                // NOP
+    OPCODE(CPX_abs,2,0),
+    OPCODE(SBC_abs,2,0),            // SBC absolute
+    OPCODE(INC_abs,2,0),            // INC absolute
+
+    OPCODE(BEQ_rel,1,0),
+    OPCODE(SBC_ind_Y,1,0),          // SBC (zeropage), Y
+    OPCODE(SBC_zpg_X,1,0),
+    OPCODE(INC_zpg_X,1,0),          // INC zeropage, X
+    OPCODE(SED,0,0),
+    OPCODE(SBC_abs_Y,2,0),          // SBC absolute, Y
+    OPCODE(SBC_abs_X,2,0),
+    OPCODE(INC_abs_X,2,0)// INC absolute, X
 };
 
 #define INSTRUCTION_COUNT   (sizeof(InstructionTable)/sizeof(InstructionTable[0]))
--- instructions_impl.cpp
+++ instructions_impl.cpp
@@ -1,6 +1,7 @@
 #ifndef INSTRUCTIONS_IMPL_CPP
 
 #define IMPLEMENT(OpCode)  void OpCode##_func(cpu_t *Cpu, ram_t *Ram, u8 *Data)
+#define NOT_IMPLEMENTED(OpCode)   IMPLEMENT(OpCode)
 
 static void
 CheckAndSetFlags(cpu_t *Cpu)
@@ -74,6 +75,166 @@ IMPLEMENT(PHA)
     PushStack(Cpu, Ram, Cpu->A);
 }
 
+NOT_IMPLEMENTED(ORA_im){}             // ORA immediate
+NOT_IMPLEMENTED(ASL){}                // ASL accumulator
+NOT_IMPLEMENTED(ORA_abs ){}
+NOT_IMPLEMENTED(ASL_abs){}            // ASL absolute
+
+NOT_IMPLEMENTED(BPL_rel ){}
+NOT_IMPLEMENTED(ORA_ind_Y){}          // ORA (zeropage), Y
+NOT_IMPLEMENTED(ORA_zpg_X ){}
+NOT_IMPLEMENTED(ASL_zpg_X){}          // ASL zeropage, X
+NOT_IMPLEMENTED(CLC ){}
+NOT_IMPLEMENTED(ORA_abs_Y){}          // ORA absolute, Y
+NOT_IMPLEMENTED(ORA_abs_X ){}
+NOT_IMPLEMENTED(ASL_abs_X){}          // ASL absolute, X
+
+NOT_IMPLEMENTED(JSR ){}
+NOT_IMPLEMENTED(AND_X_ind){}          // AND (zeropage, X)
+NOT_IMPLEMENTED(BIT_zpg ){}
+NOT_IMPLEMENTED(AND_zpg){}            // AND zeropage
+NOT_IMPLEMENTED(ROL_zpg){}            // ROL zeropage
+NOT_IMPLEMENTED(PLP ){}
+NOT_IMPLEMENTED(AND_im){}             // AND immediate
+NOT_IMPLEMENTED(ROL){}                // ROL accumulator,
+NOT_IMPLEMENTED(BIT_abs ){}
+NOT_IMPLEMENTED(AND_abs){}            // AND absolute
+NOT_IMPLEMENTED(ROL_abs){}            // ROL absolute
+
+NOT_IMPLEMENTED(BMI_rel ){}
+NOT_IMPLEMENTED(AND_ind_Y){}          // AND (zeropage), Y
+NOT_IMPLEMENTED(AND_zpg_X ){}
+NOT_IMPLEMENTED(ROL_zpg_X){}          // ROL zeropage, X
+NOT_IMPLEMENTED(SEC ){}
+NOT_IMPLEMENTED(AND_abs_Y){}          // AND absolute, Y
+NOT_IMPLEMENTED(AND_abs_X ){}
+NOT_IMPLEMENTED(ROL_abs_X){}          // ROL absolute, X
+
+NOT_IMPLEMENTED(RTI ){}
+NOT_IMPLEMENTED(EOR_X_ind){}          // EOR (zeropage, X)
+NOT_IMPLEMENTED(EOR_zpg ){}
+NOT_IMPLEMENTED(LSR_zpg){}            // LSR zeropage
+NOT_IMPLEMENTED(EOR_im){}             // EOR immediate
+NOT_IMPLEMENTED(LSR){}                // LSR accumulator
+NOT_IMPLEMENTED(JMP_abs ){}
+NOT_IMPLEMENTED(EOR_abs){}            // EOR absolute
+NOT_IMPLEMENTED(LSR_abs){}            // LSR absolute
+
+NOT_IMPLEMENTED(BVC_rel){}
+NOT_IMPLEMENTED(EOR_ind_Y){}          // EOR (zeropage), Y
+NOT_IMPLEMENTED(EOR_zpg_X){}
+NOT_IMPLEMENTED(LSR_zpg_X){}          // LSR zeropage, X
+NOT_IMPLEMENTED(CLI){}
+NOT_IMPLEMENTED(EOR_abs_Y){}          // EOR absolute, Y
+NOT_IMPLEMENTED(EOR_abs_X){}
+NOT_IMPLEMENTED(LSR_abs_X){}          // LSR absolute, X
+
+NOT_IMPLEMENTED(RTS){}
+NOT_IMPLEMENTED(ADC_X_ind){}          // ADC (zeropage, X)
+NOT_IMPLEMENTED(ADC_zpg){}
+NOT_IMPLEMENTED(ROR_zpg){}            // ROR zeropage
+NOT_IMPLEMENTED(PLA){}
+NOT_IMPLEMENTED(ADC_im){}             // ADC immediate
+NOT_IMPLEMENTED(ROR){}                // ROR accumulator
+NOT_IMPLEMENTED(JMP_ind){}
+NOT_IMPLEMENTED(ADC_abs){}            // ADC absolute
+NOT_IMPLEMENTED(ROR_abs){}            // ROR absolute
+
+NOT_IMPLEMENTED(BVS_rel){}
+NOT_IMPLEMENTED(ADC_ind_Y){}          // ADC (zeropage), Y
+NOT_IMPLEMENTED(ADC_zpg_X){}
+NOT_IMPLEMENTED(ROR_zpg_X){}          // ROR zeropage, X
+NOT_IMPLEMENTED(SEI){}
+NOT_IMPLEMENTED(ADC_abs_Y){}          // ADC absolute, Y
+NOT_IMPLEMENTED(ADC_abx_X){}
+NOT_IMPLEMENTED(ROR_abs_X){}          // ROR absolute, X
+
+NOT_IMPLEMENTED(STA_X_ind){}
+NOT_IMPLEMENTED(STY_zpg){}
+NOT_IMPLEMENTED(STA_zpg){}            // STA zeropage
+NOT_IMPLEMENTED(STX_zpg){}            // STX zeropage
+NOT_IMPLEMENTED(DEY){}
+NOT_IMPLEMENTED(TXA){}
+NOT_IMPLEMENTED(STY_abs){}
+NOT_IMPLEMENTED(STA_abs){}            // STA absolute,
+NOT_IMPLEMENTED(STX_abs){}            // STX absolute
+
+NOT_IMPLEMENTED(BCC_rel){}
+NOT_IMPLEMENTED(STA_ind_Y){}          // STA (zeropage), Y
+NOT_IMPLEMENTED(STY_zpg_X){}
+NOT_IMPLEMENTED(STA_zpg_X){}          // STA zeropage, X
+NOT_IMPLEMENTED(STX_zpg_Y){}          // STX zeropage, Y
+NOT_IMPLEMENTED(TYA){}
+NOT_IMPLEMENTED(STA_abs_Y){}          // STA absolute, Y
+NOT_IMPLEMENTED(TXS){}                // TXS
+NOT_IMPLEMENTED(STA_abs_X){}
+
+NOT_IMPLEMENTED(LDY_im){}
+NOT_IMPLEMENTED(LDA_X_ind){}          // LDA (zeropage, X)
+NOT_IMPLEMENTED(LDX_im){}             // LDX immediate
+NOT_IMPLEMENTED(LDY_zpg){}
+NOT_IMPLEMENTED(LDA_zpg){}            // LDA zeropage
+NOT_IMPLEMENTED(LDX_zpg){}            // LDX zeropage
+NOT_IMPLEMENTED(TAY){}
+NOT_IMPLEMENTED(LDA_im){}             // LDA immediate
+NOT_IMPLEMENTED(TAX){}                // TAX
+NOT_IMPLEMENTED(LDY_abs){}
+NOT_IMPLEMENTED(LDA_abs){}            // LDA absolute
+NOT_IMPLEMENTED(LDX_abs){}            // LDX absolute
+
+NOT_IMPLEMENTED(BCS_rel){}
+NOT_IMPLEMENTED(LDA_ind_Y){}          // LDA (zeropage), Y
+NOT_IMPLEMENTED(LDY_zpg_X){}
+NOT_IMPLEMENTED(LDA_zpg_X){}          // LDA zeropage, X
+NOT_IMPLEMENTED(LDX_zpg_Y){}          // LDX zeropage, Y
+NOT_IMPLEMENTED(CLV){}
+NOT_IMPLEMENTED(LDA_abs_Y){}          // LDA absolute, Y
+NOT_IMPLEMENTED(TSX){}                // TSX
+NOT_IMPLEMENTED(LDY_abs_X){}
+NOT_IMPLEMENTED(LDA_abs_X){}          // LDA absolute, X
+NOT_IMPLEMENTED(LDX_abs_Y){}          // LDX absolute, Y
+
+NOT_IMPLEMENTED(CPY_im){}
+NOT_IMPLEMENTED(CMP_X_ind){}          // CMP (zeropage, X)
+NOT_IMPLEMENTED(CPY_zpg){}
+NOT_IMPLEMENTED(CMP_zpg){}            // CMP zeropage
+NOT_IMPLEMENTED(DEC_zpg){}            // DEC zeropage
+NOT_IMPLEMENTED(INY){}
+NOT_IMPLEMENTED(CMP_im){}             // CMP immediate
+NOT_IMPLEMENTED(DEX){}                // DEX
+NOT_IMPLEMENTED(CPY_abs){}
+NOT_IMPLEMENTED(CMP_abs){}            // CMP absolute
+NOT_IMPLEMENTED(DEC_abs){}            // DEC absolute
+
+NOT_IMPLEMENTED(BNE_rel){}
+NOT_IMPLEMENTED(CMP_ind_Y){}          // CMP (zeropage), Y
+NOT_IMPLEMENTED(CMP_zpg_X){}
+NOT_IMPLEMENTED(DEC_zpg_X){}          // DEC zeropage, X
+NOT_IMPLEMENTED(CLD){}
+NOT_IMPLEMENTED(CMP_abs_Y){}          // CMP absolute, Y
+NOT_IMPLEMENTED(CMP_abs_X){}
+NOT_IMPLEMENTED(DEC_abs_X){}          // DEC absolute, X
+
+NOT_IMPLEMENTED(CPX_im){}
+NOT_IMPLEMENTED(SBC_X_ind){}          // SBC (zeropage, X)
+NOT_IMPLEMENTED(CPX_zpg){}
+NOT_IMPLEMENTED(SBC_zpg){}            // SBC zeropage
+NOT_IMPLEMENTED(INC_zpg){}            // INC zeropage
+NOT_IMPLEMENTED(INX){}
+NOT_IMPLEMENTED(SBC_im){}             // SBC immediate
+NOT_IMPLEMENTED(NOP){}                // NOP
+NOT_IMPLEMENTED(CPX_abs){}
+NOT_IMPLEMENTED(SBC_abs){}            // SBC absolute
+NOT_IMPLEMENTED(INC_abs){}            // INC absolute
+
+NOT_IMPLEMENTED(BEQ_rel){}
+NOT_IMPLEMENTED(SBC_ind_Y){}          // SBC (zeropage), Y
+NOT_IMPLEMENTED(SBC_zpg_X){}
+NOT_IMPLEMENTED(INC_zpg_X){}          // INC zeropage, X
+NOT_IMPLEMENTED(SED){}
+NOT_IMPLEMENTED(SBC_abs_Y){}          // SBC absolute, Y
+NOT_IMPLEMENTED(SBC_abs_X){}
+NOT_IMPLEMENTED(INC_abs_X){}
 
 #define INSTRUCTIONS_IMPL_CPP
 #endif
