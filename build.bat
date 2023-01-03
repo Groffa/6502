5d24a5194e1bdcd87801b44ddd421cadeea663f9
Groffa <groffa>
2023-01-03 16:14:56 +0100

    Updated build.bat for VS 2022 Community Edition

--- build.bat
+++ build.bat
@@ -1,10 +1,8 @@
 @echo off
 
-REM set LIBS=user32.lib kernel32.lib gdi32.lib Winmm.lib
+set MYVC=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 
-set MYVC=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC
-
-call "%MYVC%\vcvarsall.bat" x64
+call "%MYVC%\vcvars64.bat"
 
 REM cl /Fe6502.exe /O2 6502.cpp
 cl /Zi /Fe6502_debug.exe 6502.cpp
